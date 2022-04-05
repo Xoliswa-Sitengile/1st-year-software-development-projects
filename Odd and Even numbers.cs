3f8e1c1c7588c73aa1205a59b179d3967ea74586
Xoliswa <98959246+Xoliswa-Sitengile@users.noreply.github.com>
2022-04-05 21:08:34 +0200

    Add files via upload
    This is a simple program that tells you if a number is even or an odd number.

---
+++ Odd and Even numbers.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Prac9Question1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number, answer;
+            string message;
+            Console.Write("Enter a number: ");
+            number = int.Parse(Console.ReadLine());
+            answer = number % 2; //determines remainder
+            if (answer == 0) // if remainder == zero               
+               message="Even number";
+            else
+              message= "Odd number";
+            DisplayLine(message);
+        }
+        static void DisplayLine(string message)
+        {
+            Console.WriteLine("{0}", message);
+            Console.ReadLine();
+        }
+    }
+}
